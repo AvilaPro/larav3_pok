e68b054553d3d2b30b27a3f516159e2f911add29
avilapro <mejiasavilaeduardo@gmail.com>
2020-02-24 08:23:55 -0400

    cap 11 VIEWS

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -23,7 +23,7 @@ class TrainerController extends Controller
      */
     public function create()
     {
-        //
+        return view('trainers.create');
     }
 
     /**
---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+    crossorigin="anonymous">
+    <title>Create Trainer</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Guardar</button>
+    </div>
+
+   
+</body>
+</html>
