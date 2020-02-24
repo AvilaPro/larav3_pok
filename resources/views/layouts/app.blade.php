c76eeeffa7c9beadef230d154da6db0593b68fd6
avilapro <mejiasavilaeduardo@gmail.com>
2020-02-24 09:11:33 -0400

    cap 12 BLADE

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+    crossorigin="anonymous">
+    <title>LaraPok - @yield('title')</title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    
+</body>
+</html>
--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-    crossorigin="anonymous">
-    <title>Create Trainer</title>
-</head>
-<body>
-    <div class="container">
-        <div class="form-group">
-            <label for="">Nombre</label>
-            <input type="text" class="form-control">
-        </div>
+@extends('layouts.app')
 
-        <button type="submit" class="btn btn-primary">Guardar</button>
+@section('title', 'Trainer Create')
+
+@section('content')
+    <div class="form-group">
+           <label for="">Nombre</label>
+            <input type="text" class="form-control">
     </div>
 
-   
-</body>
-</html>
+    <button type="submit" class="btn btn-primary">Guardar</button>
+    
+@endsection
+
+
