b02b0db615a71bb804f64387386510b0df7b7aff
avilapro <mejiasavilaeduardo@gmail.com>
2020-02-25 19:57:05 -0400

    cap 21 slugs

--- resources/views/trainers/show.blade.php
+++ resources/views/trainers/show.blade.php
@@ -5,7 +5,8 @@
 @section('content')
     <img style="height: 200px; width:200px; backgroud-color: #EFEFEF; margin:20px" src="/images/{{$trainer->avatar}}" alt="" class="card-img-top rounded-circle mx-auto d-block">
     <div class="text-center">
-    <h5 class="card-title">{{$trainer->name}}</h5>
+        <h5 class="card-title">{{$trainer->name}}</h5>
+        
         <p>{{$trainer->description}} </p>
     </div>
 @endsection
