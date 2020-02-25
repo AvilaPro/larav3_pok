f3fdd0a9e5527f74d280a60259a3412e1745f99c
avilapro <mejiasavilaeduardo@gmail.com>
2020-02-25 15:02:39 -0400

    cap 19 Vista de los Trainers

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -6,8 +6,8 @@
     <div class="row">
         @foreach ($trainers as $trainer)
             <div class="col-sm">
-                <div class="card" style="width: 18rem;">
-                <img src="images/{{$trainer->avatar}}" alt="" class="card-img-top">
+                <div class="card text-center" style="width: 18rem; margin-top: 70px">
+                <img style="height: 150px; width:150px; backgroud-color: #EFEFEF; margin:20px" src="images/{{$trainer->avatar}}" alt="" class="card-img-top rounded-circle mx-auto d-block">
                     <div class="card-body">
                       <h5 class="card-title">{{$trainer->name}}</h5>
                       <p class="card-text">{{$trainer->description}}</p>
