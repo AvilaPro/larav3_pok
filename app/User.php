9d3c3a13bb562adc6394aad5b861cb8e86d4b4dd
avilapro <mejiasavilaeduardo@gmail.com>
2020-03-16 17:13:16 -0400

    cap 36 roles de user-  logic validacion

--- app/User.php
+++ app/User.php
@@ -14,6 +14,34 @@ class User extends Authenticatable
         return $this->belongsToMany('LaraPok\Role');
     }
 
+    public function authorizeRoles($roles){
+        if($this->hasAnyRole($roles)){
+            return true;
+        }
+        abort(401, 'This action unauthorized');
+    }
+
+    public function hasAnyRole($roles){
+        if(is_array(($roles))){
+            foreach ($roles as $role){
+                if($this->hasRole($role)){
+                    return true;
+                }
+            }
+        }else{
+            if($this->hasRole($roles)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole($role){
+        if($this->roles()->where('name',$role->first())){
+            return true;
+        }
+        return false;
+    }
     /**
      * The attributes that are mass assignable.
      *
